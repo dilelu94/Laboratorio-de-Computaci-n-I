a75d41dae3842a36fe12eb4555ac77e80255baed
dilelu94 <diego.leonel.luque@gmail.com>
2023-03-30 09:38:25 -0300

    blank ej10

---
+++ TP N°1 - Estructura de Secuencia/Ejercicios/Ej10.cpp
@@ -0,0 +1,19 @@
+///Ejercicio:
+///Autor: Diego Leonel Luque
+///Fecha: 30
+///Comentario:
+
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+/*
+
+
+*/
+  cout<<endl;
+  system("pause");
+  return 0;
+}
