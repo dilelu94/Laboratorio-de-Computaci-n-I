3b6ea562a838de034bc980614effb28c59ab9944
dilelu94 <diego.leonel.luque@gmail.com>
2023-03-30 09:46:53 -0300

    Ej 10 completado

--- TP N°1 - Estructura de Secuencia/Ejercicios/Ej10.cpp
+++ TP N°1 - Estructura de Secuencia/Ejercicios/Ej10.cpp
@@ -1,6 +1,6 @@
-///Ejercicio:
+///Ejercicio: Ej10IngresarHorasYMostrarCuantosDiasYHorasEquivalen
 ///Autor: Diego Leonel Luque
-///Fecha: 30
+///Fecha: 30/03/2023
 ///Comentario:
 
 #include<iostream>
@@ -9,11 +9,14 @@
 using namespace std;
 
 int main(){
-/*
+  int horas, dias, horasRestantes;
+  cout << "Ingrese horas: ";
+  cin >> horas;
+  dias = horas / 24;
+  horasRestantes = horas % 24;
+  cout << "Dias: " << dias << endl;
+  cout << "Horas: "<< horasRestantes << endl;
 
-
-*/
-  cout<<endl;
   system("pause");
   return 0;
 }
