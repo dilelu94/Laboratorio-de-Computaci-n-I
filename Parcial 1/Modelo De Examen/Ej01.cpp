20563a47b528e1f0add1062f24944dc166d1d286
dilelu94 <diego.leonel.luque@gmail.com>
2023-05-11 18:12:49 -0300

    Modelo de parcial y ej1 comienzo

---
+++ Parcial 1/Modelo De Examen/Ej01.cpp
@@ -0,0 +1,19 @@
+/// Ejercicio: Ej01
+/// Autor: Diego Leonel Luque
+/// Fecha: 11/05/2023
+/// Comentario:
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+  /*
+  
+  */
+  
+  system("pause");
+  return 0;
+}
