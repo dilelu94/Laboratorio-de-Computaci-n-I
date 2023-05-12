9c449893e4bee93b6d93141364165e20b7a2b2b0
dilelu94 <diego.leonel.luque@gmail.com>
2023-05-11 22:41:28 -0300

    initialized

---
+++ Parcial 1/luque_leonel4.cpp
@@ -0,0 +1,19 @@
+/// Ejercicio: Ej4 del parcial 1
+/// Autor: Diego Leonel Luque
+/// Fecha: 11/05/2023
+/// Comentario:
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+  /*
+  
+  */
+  
+  system("pause");
+  return 0;
+}
