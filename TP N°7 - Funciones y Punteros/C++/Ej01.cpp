3446628973a2c8c4fa8651c2922c9cde3fa6ba57
dilelu94 <diego.leonel.luque@gmail.com>
2023-05-27 00:11:29 -0300

    tp7

---
+++ TP N°7 - Funciones y Punteros/C++/Ej01.cpp
@@ -0,0 +1,30 @@
+/// Ejercicio: Ej01IngresarUnNumeroYMostrarlo
+/// Autor: Diego Leonel Luque
+/// Fecha: 26/05/2023
+/// Comentario:
+
+#include "funciones.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void mostrarTresVeces(int numero)
+{
+  cout << numero << endl;
+  cout << numero << endl;
+  cout << numero << endl;
+  return;
+}
+
+int main()
+{
+  int numero;
+  cout << "Ingrese un numero: ";
+  cin >> numero;
+
+  mostrarTresVeces(numero);
+
+  system("pause");
+  return 0;
+}
---
+++ TP N°7 - Funciones y Punteros/C++/Ej02.cpp
@@ -0,0 +1,20 @@
+/// Ejercicio: Ej02
+/// Autor: Diego Leonel Luque
+/// Fecha: 27/05/2023
+/// Comentario:
+
+#include "funciones.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+  /*
+  
+  */
+  
+  system("pause");
+  return 0;
+}
