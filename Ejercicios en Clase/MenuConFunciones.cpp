93bdc46d9f524adf1a34716bef6d1c1f2f14e2e5
dilelu94 <diego.leonel.luque@gmail.com>
2023-06-03 16:44:50 -0300

    format fix

--- Ejercicios en Clase/MenuConFunciones.cpp
+++ Ejercicios en Clase/MenuConFunciones.cpp
@@ -23,32 +23,39 @@ int main()
     cout << "4. Mostrar Valor del Maximo" << endl;
     cout << "0. Fin del Programa" << endl;
     cout << "=====================" << endl;
+
     cout << "Ingrese una Opcion: ";
     cin >> opcion;
     system("cls");
+
     switch (opcion)
     {
     case 0:
       return 0;
       break;
+
     case 1:
       // cargarVector(vector, 5);
       cargarAleatorio(vector, 5, 100);
       break;
+
     case 2:
       mostrarVector(vector, 5);
       endl(cout);
       system("pause");
       break;
+
     case 3:
       posicionMax = maximoVector(vector, 5);
       cout << "El maximo se encuentra en la posicion: " << posicionMax << endl;
       system("pause");
       break;
+
     case 4:
       cout << "El maximo valor del vector es: " << maximoValorDelVector(vector, 5) << endl;
       system("pause");
       break;
+
     default:
       cout << "Opcion Incorrecta" << endl;
       cout << endl;
